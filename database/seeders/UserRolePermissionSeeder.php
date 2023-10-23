d547ec6c50318733476de88b0dc2dd674e6c842b
olivvmj <jannahvvmj@gmail.com>
2023-10-23 13:32:34 +0700

    Create Multi Level User Role

---
+++ database/seeders/UserRolePermissionSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+
+class UserRolePermissionSeeder extends Seeder
+{
+    public function run()
+    {
+        $default_user_value = [
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+
+        DB::beginTransaction();
+
+        try {
+            $superAdmin = User::create(array_merge([
+                'name' => 'Super Admin',
+                'username' => 'SuperAdmin',
+            ], $default_user_value));
+
+            $operator = User::create(array_merge([
+                'name' => 'Operator',
+                'username' => 'operator1',
+            ], $default_user_value));
+
+            $client = User::create(array_merge([
+                'name' => 'Peminjam',
+                'username' => 'Peminjam',
+            ], $default_user_value));
+
+            $role_superAdmin = Role::create(['name' => 'superAdmin']);
+            $role_operator = Role::create(['name' => 'operator']);
+            $role_client = Role::create(['name' => 'client']);
+
+            $permission = Permission::create(['name' => 'read role']);
+            $permission = Permission::create(['name' => 'create role']);
+            $permission = Permission::create(['name' => 'update role']);
+            $permission = Permission::create(['name' => 'delete role']);
+
+            $superAdmin->assignRole('superAdmin');
+            $operator->assignRole('operator');
+            $client->assignRole('client');
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollback();
+            throw $th;
+        }
+    }
+}
